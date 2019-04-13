490454aa85ba74220a2c7c26dcaacc812b2917d8
Bhavul Gauri <bhavul93@gmail.com>
2019-04-13 19:13:02 +0530

    fix json serialisable error

--- commands.py
+++ commands.py
@@ -47,6 +47,8 @@ def check_dm_and_respond():
     # Get the last DM
     DMs = api.get_direct_messages(count=1, skip_status='true')
 
+    # print(DMs)
+
     if 'events' in DMs and len(DMs['events']) > 0:
         msg = DMs['events'][0]
         if 'message_create' in msg and 'message_data' in msg['message_create'] and 'text' in msg['message_create'][
@@ -57,19 +59,19 @@ def check_dm_and_respond():
 
             if 'ip' in text:
                 output = subprocess.check_output("ifconfig | grep inet", shell=True)
-                event_data = get_formatted_data(receiverTwitterId, output)
+                event_data = get_formatted_data(receiverTwitterId, str(output))
                 api.send_direct_message(**event_data)
                 print('Sent IP address')
 
             if 'ifconfig' in text:
                 output = subprocess.check_output("ifconfig", shell=True)
-                event_data = get_formatted_data(receiverTwitterId, output)
+                event_data = get_formatted_data(receiverTwitterId, str(output))
                 api.send_direct_message(**event_data)
                 print('Sent ifconfig')
 
             if 'wifi' in text or 'iwconfig' in text:
-                output = subprocess.check_output("iwconfig", shell=True)
-                event_data = get_formatted_data(receiverTwitterId, output)
+                output = subprocess.check_output("iwgetid", shell=True)
+                event_data = get_formatted_data(receiverTwitterId, str(output))
                 api.send_direct_message(**event_data)
 
             else:
